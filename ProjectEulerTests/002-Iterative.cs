52aad2ba4774207812d92b4b9c832f93a37e2721
Ben Crinion <ben.crinion@sorted.com>
2020-01-12 23:13:28 +0000

    Iterative version of even fibonacci numbers

---
+++ ProjectEulerTests/002-Iterative.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ProjectEulerTests
+{
+    public class Euler002Iterative
+    {
+        private readonly ITestOutputHelper output;
+
+        public Euler002Iterative(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+        private static Dictionary<long, long> calculatedFibonacciNumbers = new Dictionary<long, long>()
+        {
+            [0] = 0,
+            [1] = 1,
+            [2] = 1
+        };
+
+        [Theory]
+        [InlineData(10, 10)]
+        [InlineData(400, 188)]
+        [InlineData(4000000, 4613732)]
+        public void SumEvenFibonacciNumbers(long upTo, long expected)
+        {
+            long n = 0;
+            long x = 0;
+            long actual = 0;
+            while ((x = GetNthFibonacciNumber(n)) < upTo)
+            {
+                if (x % 2 == 0)
+                {
+                    actual += x;
+                }
+                n++;
+            }
+
+            Assert.Equal(expected, actual);
+        }
+
+        private static long GetNthFibonacciNumber(long n)
+        {
+            if (calculatedFibonacciNumbers.ContainsKey(n))
+            {
+                return calculatedFibonacciNumbers[n];
+            }
+            else
+            {
+                var f = calculatedFibonacciNumbers[n - 1] + calculatedFibonacciNumbers[n - 2];
+                calculatedFibonacciNumbers[n] = f;
+                return f;
+            }
+        }
+
+    }
+}
